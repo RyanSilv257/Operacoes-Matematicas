c36a6f042f00e3309777ebb04094eda7960664e1
RyanSilv257 <130668947+RyanSilv257@users.noreply.github.com>
2023-08-11 13:55:25 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 // Declaração das variaveis 
-
 let operacao = prompt("Informe a operação desejada: ")
 let numero1 = Number(prompt("Informe o primeiro número: "))
 let numero2 = Number(prompt("Informe o segundo número: "))
+
+// Verifica a operação matematica selecionada
+if (operacao = "+") {
+    let resultado = numero1 + numero2
+    console.log("O resultado da soma é " + resultado + ".")
+}
+else if (operacao = "-") {
+    let resultado = numero1 - numero2
+    console.log("O resultado da subtração é " + resultado + ".")
+}
+else if (operacao = "*") {
+    let resultado = numero1 * numero2
+    console.log("O resultado da multiplicação é " + resultado + ".")
+}
+else if (operacao = "/") {
+    let resultado = numero1 / numero2
+    console.log("O resultado da divisão é " + resultado + ".")
+}
