50374d6223d60bccce99c399626394ca779f7824
RyanSilv257 <130668947+RyanSilv257@users.noreply.github.com>
2023-08-11 13:47:05 -0300

    commit

---
+++ main.js
@@ -0,0 +1,5 @@
+// Declaração das variaveis 
+
+let operacao = prompt("Informe a operação desejada: ")
+let numero1 = Number(prompt("Informe o primeiro número: "))
+let numero2 = Number(prompt("Informe o segundo número: "))
